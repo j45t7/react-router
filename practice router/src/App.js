f413181c7b277ea235fd7e968c6327aae61de143
Justyna Marciniak <justy.marciniak@gmail.com>
2021-10-30 17:25:51 +0900

    add 404 page

--- practice router/src/App.js
+++ practice router/src/App.js
@@ -3,6 +3,7 @@ import SingleQuote from './pages/SingleQuote'
 import AllQuotes from './pages/AllQuotes'
 import NewQuote from './pages/NewQuote'
 import Layout from './components/layout/Layout'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route path='/new-quote'>
           <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
